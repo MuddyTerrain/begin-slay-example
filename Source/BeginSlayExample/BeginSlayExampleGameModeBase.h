9b735cd1bac04deae89b61708afee993b25fe5d8
Prajwal Shetty <prajwalshetty3000@gmail.com>
2025-09-06 14:13:55 +0100

    first commit

---
+++ Source/BeginSlayExample/BeginSlayExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeginSlayExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEGINSLAYEXAMPLE_API ABeginSlayExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
